fff41f511886148a1649c0bd1a3cc703504ebc83
ManuelNC250 <manuelnavarrocortes@elpuig.xeill.net>
2026-04-22 20:05:58 +0200

    config per a desplegament fullstack

--- src/main/java/com/example/conflicttracker/controller/ConflictController.java
+++ src/main/java/com/example/conflicttracker/controller/ConflictController.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/conflicts")
-@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173"}) // Permitir solicitudes desde el frontend
 public class ConflictController {
 
     @Autowired
